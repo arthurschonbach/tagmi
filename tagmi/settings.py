c6c9360fc7d69acb972c5f6bc87c39e5fa395ad6
arthurschonbach <arthur.schonbach@gmail.com>
2025-05-17 21:31:10 +1000

    templates

--- tagmi/settings.py
+++ tagmi/settings.py
@@ -100,7 +100,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or your configured directory
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
