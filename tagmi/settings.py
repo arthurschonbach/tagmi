52ed3a5e9722a81467d6431c1580b39b2cdc2ecd
arthurschonbach <arthur.schonbach@gmail.com>
2025-05-17 18:02:05 +1000

    req

--- tagmi/settings.py
+++ tagmi/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
