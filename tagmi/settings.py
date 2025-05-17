f4fee65e070bd3ef96bae136dec68419e39c67db
arthurschonbach <arthur.schonbach@gmail.com>
2025-05-17 20:13:01 +1000

    templates

--- tagmi/settings.py
+++ tagmi/settings.py
@@ -37,13 +37,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
